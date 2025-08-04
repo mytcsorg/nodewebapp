930bf2ce4616c7ab0d1087f0c095414059695788
kannan <kannans89@hotmail.com>
2025-08-04 21:42:51 +0530

    index changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
-res.send('Hello, this is my web application!');
+res.send('<h1>Hello, this is my web application!</h1>');
 });
 app.listen(port, () => {
 console.log(`Server is running at http://localhost:${port}`);
